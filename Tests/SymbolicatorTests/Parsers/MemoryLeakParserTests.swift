636edb1848cf8f4b13041f5c394af92f4755880e
berik <berik@mobile.dev>
2022-08-01 19:33:18 +0400

    Test symbolication

--- Tests/SymbolicatorTests/Parsers/MemoryLeakParserTests.swift
+++ Tests/SymbolicatorTests/Parsers/MemoryLeakParserTests.swift
@@ -14,6 +14,8 @@ final class MemoryLeakParserTests: XCTestCase {
             dsymPath: TestResources().dsymUrl.appendingPathComponent("Contents/Resources/DWARF/MemoryLeakingApp").path,
             arch: "x86_64")
         let result = runner.run(on: string)
-        print(result)
+        
+        XCTAssert(result.contains("0x102d6026e main (in MemoryLeakingApp) (<compiler-generated>:0)"))
+        XCTAssert(result.contains("0x102d62777 closure #2 in closure #1 in closure #1 in closure #1 in ContentView.body.getter (in MemoryLeakingApp) (ContentView.swift:40)"))
     }
 }
