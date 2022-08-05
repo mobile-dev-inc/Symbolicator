b63ae263198548fce627f520c64b1418837e16c2
berik <berik@mobile.dev>
2022-08-05 18:22:12 +0400

    Improve error handling

--- Sources/Symbolicator/SymbolicatorApp.swift
+++ Sources/Symbolicator/SymbolicatorApp.swift
@@ -18,74 +18,81 @@ struct SymbolicatorApp: ParsableCommand {
     @Flag(help: "JSON output")
     var json = false
     
-    mutating func run() throws {
-        printStderr("Symbolicator, arguments:")
-        printStderr("   dsym: \(dsymFile ?? "none")")
-        printStderr("   file: \(inputFileArgument)")
-        
+    func run() {
+        do {
+            try symbolicate()
+        } catch let error {
+            printStderr("Error:", error)
+            Foundation.exit(1)
+        }
+    }
+
+    func symbolicate() throws {
         let inputData: Data
         if inputFileArgument == "-" {
-            printStderr("Reading from stdin")
-            
             guard let data = try FileHandle.standardInput.readToEnd() else {
-                throw ReadError()
+                throw SymbolicatorAppError("Error reading from stdin")
             }
-            
+
             inputData = data
         } else {
             let url = URL(fileURLWithPath: inputFileArgument)
             guard FileManager().fileExists(atPath: inputFileArgument) else {
                 throw ValidationError("Input file not found at \(inputFileArgument)")
             }
-            
+
             inputData = try Data(contentsOf: url)
         }
 
+        guard let contents = String(data: inputData, encoding: .utf8) else { fatalError() }
 
-        parse(inputData)
-    }
-    
-    func parse(_ data: Data) {
-        guard let contents = String(data: data, encoding: .utf8) else { fatalError() }
-            
         if contents.contains("leaks Report Version") {
-            let result: String
-            if let dsymFile = dsymFile {
-                let symbolicator = MemoryLeakReportSymbolicator(contents)
-                let runner = SymbolicatorRunner(symbolicator: symbolicator, dsymPath: dsymFile, arch: arch)
-                result = runner.run(on: contents)
-            } else {
-                result = contents
-            }
-            
-            if json {
-                do {
-                    let report = try MemoryLeakReportParser().parse(result)
-                    let encoder = JSONEncoder()
-                    let encoded = try encoder.encode(report)
-
-                    print(String(data: encoded, encoding: .utf8)!)
-                    
-                } catch {
-                    printStderr("Failed to parse memory leak report \(error.localizedDescription)")
-                    printStderr(error)
-                }
-            } else {
-                print(result)
-            }
-            
+            try symbolicateMemoryLeakReport(contents)
         } else if contents.contains("Crashed Thread:") {
-            if let dsymFile = dsymFile {
-                let appName = appName ?? "CrashReporter"
-                let symbolicator = CrashReportSymbolicator(contents: contents, appName: appName)
-                let runner = SymbolicatorRunner(symbolicator: symbolicator, dsymPath: dsymFile, arch: arch)
-                let result = runner.run(on: symbolicator.swappedAppCrashFileContents)
-                print(result)
-            } else {
-                print(contents)
-            }
+            symbolicateLegacyCrashReport(contents)
+        } else {
+            throw SymbolicatorAppError("Unrecognised file format")
+        }
+    }
+
+    fileprivate func symbolicateMemoryLeakReport(_ contents: String) throws {
+        let result: String
+        if let dsymFile = dsymFile {
+            let symbolicator = MemoryLeakReportSymbolicator(contents)
+            let runner = SymbolicatorRunner(symbolicator: symbolicator, dsymPath: dsymFile, arch: arch)
+            result = runner.run(on: contents)
+        } else {
+            result = contents
+        }
+
+        guard json else {
+            print(result)
+            return
+        }
+
+        let report = try MemoryLeakReportParser().parse(result)
+        let encoder = JSONEncoder()
+        let encoded = try encoder.encode(report)
+        
+        print(String(data: encoded, encoding: .utf8)!)
+    }
+
+    fileprivate func symbolicateLegacyCrashReport(_ contents: String) {
+        if let dsymFile = dsymFile {
+            let appName = appName ?? "CrashReporter"
+            let symbolicator = CrashReportSymbolicator(contents: contents, appName: appName)
+            let runner = SymbolicatorRunner(symbolicator: symbolicator, dsymPath: dsymFile, arch: arch)
+            let result = runner.run(on: symbolicator.swappedAppCrashFileContents)
+            print(result)
+        } else {
+            print(contents)
         }
     }
 }
 
-struct ReadError: Error {}
+struct SymbolicatorAppError: Error, CustomStringConvertible {
+    let description: String
+    init(_ message: String) {
+        self.description = message
+    }
+}
