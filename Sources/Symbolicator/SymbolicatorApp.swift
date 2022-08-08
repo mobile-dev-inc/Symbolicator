f2fd69fffe8601f5c058c3533d1428863c695fc3
berik <berik@mobile.dev>
2022-08-08 14:19:21 +0200

    Add support for symbolizing IPS files

--- Sources/Symbolicator/SymbolicatorApp.swift
+++ Sources/Symbolicator/SymbolicatorApp.swift
@@ -1,5 +1,6 @@
 import Foundation
 import ArgumentParser
+import GenericJSON
 
 @main
 struct SymbolicatorApp: ParsableCommand {
@@ -50,6 +51,10 @@ struct SymbolicatorApp: ParsableCommand {
             try symbolicateMemoryLeakReport(contents)
         } else if contents.contains("Crashed Thread:") {
             try symbolicateLegacyCrashReport(contents)
+        } else if contents.hasPrefix("{"),
+            let firstNewline = contents.firstIndex(of: "\n"),
+                  contents[contents.index(before: firstNewline)] == "}" {
+            try symbolicateIPSCrashReport(from: inputData)
         } else {
             throw SymbolicatorAppError("Unrecognised file format")
         }
@@ -82,10 +87,59 @@ struct SymbolicatorApp: ParsableCommand {
             let appName = appName ?? "CrashReporter"
             let symbolicator = CrashReportSymbolicator(contents: contents, appName: appName)
             let runner = SymbolicatorRunner(symbolicator: symbolicator, dsymPath: dsymFile, arch: arch)
+            let result = try runner.run(on: symbolicator.swappedAppCrashFileContents)
             print(result)
         } else {
             print(contents)
         }
+    }
+
+    fileprivate func symbolicateIPSCrashReport(from data: Data) throws {
+        let newline = Character("\n").asciiValue!
+        let newlineIndex = data.firstIndex(of: newline)!
+
+        let decoder = JSONDecoder()
+        let header = try decoder.decode([String: JSON].self, from: data[..<newlineIndex])
+        var body = try decoder.decode([String: JSON].self, from: data[newlineIndex...])
+
+        for threadIndex in body["threads"]!.arrayValue!.indices {
+            let thread = body["threads"]!.arrayValue![threadIndex]
+            let frames = thread["frames"]!.arrayValue!
+
+            for frameIndex in frames.indices {
+                let frame = frames[frameIndex].objectValue!
+                if frame["symbol"]?.stringValue != nil { continue }
+
+                let usedImages = body["usedImages"]!.arrayValue!
+                let imageIndex = Int(frame["imageIndex"]!.doubleValue!)
+                let image = usedImages[imageIndex].objectValue!
+                let base = Int(image["base"]!.doubleValue!)
+
+                let imageOffset = Int(frame["imageOffset"]!.doubleValue!)
+                let loadAddress = String(base, radix: 16, uppercase: false)
+                let address = String(base + imageOffset, radix: 16, uppercase: false)
+
+                let symbol = try atos(
+                    dsymFile!,
+                    arch: arch,
+                    loadAddress: "0x" + loadAddress,
+                    address: "0x" + address)
+
+                body["threads"]!
+                    .arrayValue![threadIndex]["frames"]!
+                    .arrayValue![frameIndex]["symbol"] = try JSON(symbol)
+            }
+        }
+
+        let headerData = try JSONEncoder().encode(header)
+        let bodyEncoder = JSONEncoder()
+        bodyEncoder.outputFormatting = .prettyPrinted
+        let bodyData = try bodyEncoder.encode(body)
+
+        var result = headerData
+        result.append(contentsOf: [newline])
+        result.append(contentsOf: bodyData)
+        print(String(data: result, encoding: .utf8)!)
     }
 }
 
